d726149db1be4a4a3e0e2c67c08c457995116c15
Aloy <aloyleow91@gmail.com>
2025-03-04 12:41:52 +0800

    first commit

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+import HomeButtons from "../components/HomeButtons"
+
+
+const HomePage = () => {
+  return (
+    <section>
+      <h1>Hi, I am Aloysious Leow</h1>
+      <h1>A Software Engineer</h1>
+      <HomeButtons/>
+    </section>
+  )
+}
+
+export default HomePage
