6ca5d39ab2f30fd28ae90a601b67d39c77916d2a
aloyleow <aloyleow91@gmail.com>
2026-03-29 22:48:09 +0800

    feat: create not found component

---
+++ src/components/page/not-found/NotFound.tsx
@@ -0,0 +1,49 @@
+import { useLocation, useRouter } from "@tanstack/react-router";
+import cn from "../../../locale/cn/not_found_cn.json";
+import en from "../../../locale/en/not_found_en.json";
+import my from "../../../locale/my/not_found_my.json";
+import { useLanguage } from "../../../state/language/useLanguage";
+import type { LocaleSettingType } from "../../../types/state.types";
+import { Information } from "../../ui/information/Information";
+import styles from "./NotFound.module.css";
+
+type ContentType = {
+  oops: string;
+  full: string;
+  part: string;
+};
+
+const localeSetting: LocaleSettingType<ContentType> = {
+  en,
+  cn,
+  my,
+};
+
+export default function NotFound() {
+  const location = useLocation();
+  const router = useRouter();
+  const { languageDetect } = useLanguage();
+
+  return (
+    <section className={styles.container}>
+      <header className={styles.upperlimit}>
+        <h1>{localeSetting[languageDetect].oops}</h1>
+      </header>
+      <main className={styles.middlelimit}>
+        <p>{localeSetting[languageDetect].full}</p>
+        <Information
+          notice={`${location.pathname} ${localeSetting[languageDetect].part}`}
+        />
+      </main>
+      <footer>
+        <button
+          type="button"
+          onClick={() => router.navigate({ to: "/" })}
+          className="button-as-button"
+        >
+          Home
+        </button>
+      </footer>
+    </section>
+  );
+}
