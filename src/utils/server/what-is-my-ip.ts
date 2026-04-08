c94046eb39fc10e1b3947ad26c59c34344e47e22
aloyleow <aloyleow91@gmail.com>
2026-04-08 22:30:42 +0800

    fix: use all ip

--- src/utils/server/what-is-my-ip.ts
+++ src/utils/server/what-is-my-ip.ts
@@ -8,7 +8,7 @@ export const whatIsMyIP = createServerFn().handler(async () => {
 
   const headers = Object.fromEntries(request.headers.entries());
 
-  const forwardIp = headers["x-forwarded-for"]?.split(",")[0] ?? "";
+  const forwardIp = headers["x-forwarded-for"]?.toString() ?? "";
   const xRealIp = headers["x-real-ip"] ?? "";
   const cfConnectingIp = headers["cf-connecting-ip"] ?? "";
   // This runs only on the server
